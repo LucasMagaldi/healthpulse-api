5dc08b54ad96ab66a6c865c814f54a420f751458
Lucas Magaldi <lucas.magaldi@hotmail.com>
2024-11-18 22:42:29 -0300

    feat: config prisma

--- src/server.ts
+++ src/server.ts
@@ -5,5 +5,5 @@ app.listen({
     host: '0.0.0.0',
     port: env.PORT
 }).then(() => {
-        console.log(`*** SERVER RUNNING AT ${env.PORT} ***`)
+    console.log(`*** SERVER RUNNING AT ${env.PORT} ***`)
 })
